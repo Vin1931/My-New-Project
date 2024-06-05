38aabda626b1cfeca219f682127f3a3a81d8768c
shaik <shaik@LAPTOP-2U3258KH>
2024-06-05 22:07:14 +0530

    my new task is complted

---
+++ src/test/java/Adactin/Sample.java
@@ -0,0 +1,14 @@
+package Adactin;
+
+public class Sample {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+	  System.out.println("the sum of a and b is:"+a+b);
+		
+
+	}
+
+}
